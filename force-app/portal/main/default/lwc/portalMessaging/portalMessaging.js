2cff436e939f669a099c7b823de6c329b56e7a3f
Andy Haas <andy_haas@aol.com>
2025-12-11 07:20:56 -0600

    fix(portal): mark all unread messages as read on load

--- force-app/portal/main/default/lwc/portalMessaging/portalMessaging.js
+++ force-app/portal/main/default/lwc/portalMessaging/portalMessaging.js
@@ -740,7 +740,8 @@ export default class PortalMessaging extends LightningElement {
      * @description Mark unread messages as read
      */
     async markUnreadMessagesAsRead() {
-        const unreadMessages = this._messages.filter(msg => !msg.isRead && msg.isMentioned);
+        // Mark all unread messages as read on load
+        const unreadMessages = this._messages.filter(msg => !msg.isRead);
         
         for (const msg of unreadMessages) {
             try {
